2ac5d30c2f7ac1352894e3c0cca5245152e10372
tedb19 <tedb19@gmail.com>
2015-07-26 18:06:12 +0300

    Updated the viewsets to include Task and User

--- scrum/board/serializers.py
+++ scrum/board/serializers.py
@@ -5,6 +5,15 @@ from rest_framework import serializers
 from .models import Sprint, Task
 
 
+'''
+Accessing the User model this way ensures that
+in case the AUTH_USER_MODEL has been swapped out
+for another later on, we have a DRY way of doing it,
+i.e. the change only affects the settings.py file 
+'''
+User = get_user_model()
+
+
 class SprintSerializer(serializers.ModelSerializer):
 
     class Meta:
@@ -38,3 +47,19 @@ class TaskSerializer(serializers.ModelSerializer):
 
     def get_status_display(self, obj):
         return obj.get_status_display()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    '''
+    This serializer assumes that if a custom User model is used,
+    then it extends from django.contrib.auth.models.CustomUser,
+    which will always have a USERNAME_FIELD attribute, get_full_name
+    method, and is_active attribute. Also note that since get_full_name
+    is a method, the field in the serializer is marked as read-only.
+    '''
+    
+    full_name = serializers.CharField(source='get_full_name', read_only=True)
+
+    class Meta:
+        model = User
+        fields = ('id', User.USERNAME_FIELD, 'full_name', 'is_active',)
--- scrum/board/views.py
+++ scrum/board/views.py
@@ -1,11 +1,43 @@
+from django.contrib.auth import get_user_model
+
 from rest_framework import viewsets
 
-from .models import Sprint
-from .serializers import SprintSerializer
+from .models import Sprint, Task
+from .serializers import SprintSerializer, TaskSerializer, UserSerializer
+
+
+User = get_user_model()
 
 
 class SprintViewSet(viewsets.ModelViewSet):
-    '''API endpoint for listing and creating sprints'''
+    '''
+    API endpoint for listing and creating sprints.
+    This viewset automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
+    '''
 
     queryset = Sprint.objects.order_by('end')
     serializer_class = SprintSerializer
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    '''API endpoint for listing and creating tasks'''
+
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    '''
+    API endpoint for listing users.
+    The ReadOnlyModelViewSet viewset automatically
+    provides `list` and `detail` actions.
+    It does not provide 'delete' and 'create' actions
+    '''
+
+    # here we change the lookup from using the ID
+    # of the user to the username
+    lookup_field = User.USERNAME_FIELD
+    lookup_url_kwarg = User.USERNAME_FIELD
+    queryset = User.objects.order_by(User.USERNAME_FIELD)
+    serializer_class = UserSerializer
