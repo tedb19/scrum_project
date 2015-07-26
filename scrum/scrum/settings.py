eb233f2853f6f093a5d011ede47dc5df83296a33
tedb19 <tedb19@gmail.com>
2015-07-26 08:02:54 +0300

    Added the Sprint and Task model classes

--- scrum/scrum/settings.py
+++ scrum/scrum/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'scrum.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'scrum',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '3306',
     }
 }
 
